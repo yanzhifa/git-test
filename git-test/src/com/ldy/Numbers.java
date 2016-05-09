b17591ebb2f51cee3d5f74f2ab496c094c0abd9b
zhifay <zhifa.yan@gmail.com>
2016-05-09 16:34:40 +0800

    Sum values.

--- git-test/src/com/ldy/Numbers.java
+++ git-test/src/com/ldy/Numbers.java
@@ -1,13 +1,19 @@
 package com.ldy;
 
+/**
+ *  Sum valuse
+ */
 public class Numbers {
 
     int number[] = { 0, 5, 9 };
 
     public void showNumbers() {
         System.out.println("Data:");
+        int sum = 0;
         for (int i = 0; i < number.length; i++) {
             System.out.println("values: " + number[i]);
+            sum += number[i];
         }
+        System.out.println(sum);
     }
 }
