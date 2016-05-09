c01ea225b01b72bc0c6542fac977108b7539d750
yanzhifa <stariver333@163.com>
2016-05-09 13:34:17 +0800

    fix bugs

--- git-test/src/com/ldy/Hello.java
+++ git-test/src/com/ldy/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Git test.");
+        System.out.println("Fix bugs.....");
+        System.out.println("------------------");
         
         Numbers number = new Numbers();
         number.showNumbers();
