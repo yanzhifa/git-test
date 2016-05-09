85a27462a3b9fb60d4046b07c4be00d8a2ce8900
yanzhifa <stariver333@163.com>
2016-05-09 13:26:01 +0800

    Initial git test

---
+++ git-test/src/com/ldy/Hello.java
@@ -0,0 +1,12 @@
+package com.ldy;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Git test.");
+        
+        Numbers number = new Numbers();
+        number.showNumbers();
+    }
+
+}
---
+++ git-test/src/com/ldy/Numbers.java
@@ -0,0 +1,10 @@
+package com.ldy;
+
+public class Numbers {
+
+    int number[] = { 0, 5, 9 };
+
+    public void showNumbers() {
+        System.out.println("TODO...");
+    }
+}
