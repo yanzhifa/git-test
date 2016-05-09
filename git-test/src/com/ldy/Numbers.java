6e9f348e4ccc108ff978630c5935946a391806f6
zhifay <zhifa.yan@gmail.com>
2016-05-09 17:20:20 +0800

    refactor number function

--- git-test/src/com/ldy/Numbers.java
+++ git-test/src/com/ldy/Numbers.java
@@ -1,19 +1,25 @@
 package com.ldy;
 
 /**
- *  Sum valuse
+ * Sum valuse
  */
 public class Numbers {
 
-    int number[] = { -1, 0, 5, 9, 20 };
+	int number[] = { -1, 0, 5, 9, 20 };
 
-    public void showNumbers() {
-        System.out.println("Data:");
-        int sum = 0;
-        for (int i : number) {
-            System.out.println("values: " + number[i]);
-            sum += number[i];
-        }
-        System.out.println(sum);
-    }
+	public void showNumbers() {
+		System.out.println("Data:");
+		for (int i : number) {
+			System.out.println("values: " + number[i]);
+		}
+	}
+
+	public void sumNumber() {
+		int sum = 0;
+		for (int i : number) {
+			System.out.println("values: " + number[i]);
+			sum += number[i];
+		}
+		System.out.println(sum);
+	}
 }
